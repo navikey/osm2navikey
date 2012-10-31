d024a8f2aec1f20787542376654e924c10e5ac78
Konstantin.Teplitzky@gmail.com <Konstantin.Teplitzky@gmail.com@c39c68d1-9aeb-f5b5-aa9f-7212e9e21987>
2012-10-31 08:06:31 +0000

    some comments into script osm2navikey

--- 7w/!template/osm2navikey.sh
+++ 7w/!template/osm2navikey.sh
@@ -25,6 +25,13 @@ CNR=`printf $TMP | awk '{ printf $2 }'`
 for file in $FILES; do \
   if (test -s osm/$file); then \
     echo "Good: " $file;
+#   If you have more than one file - 
+#   you may need to determine the country and the region for each file 
+#   It is desirable that the files were split and 
+#   identified short names to ISO 3166-2 like this AD-02.osm (meens Andorra - Canilo)
+#   if so you may need to uncomment lines below
+#    TMP=`grep -i ${file%.*} ./iso-3166-2.csv`
+#    REG=`printf $TMP | awk '{ printf $4 }'`
      ../../cfg/osm2mp.pl --bpoly=poly/${file%.*}.poly --defaultcountry=$CNR --defaultregion=$REG --config=../../cfg/Navikey-en.cfg osm/$file > mp-navikey/${file%.*}.mp
   else
     echo "Error in file: " $file;
