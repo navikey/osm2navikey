8943d433e41e8b6ac42518686c7cefa72823c453
Konstantin.Teplitzky@gmail.com <Konstantin.Teplitzky@gmail.com@c39c68d1-9aeb-f5b5-aa9f-7212e9e21987>
2012-11-13 11:36:39 +0000

    Fix prep.sh

--- 7w/!template/prep.sh
+++ 7w/!template/prep.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 CNTR=setupcountry
-FILES='./splitit.sh ./poly/getpoly.sh ./7w/pack.sh ./7w/store.lst ./osm/get.bat ./poly/get.sh 27w.sh config.cfg osm2navikey.bat osm2navikey_one.bat';
+FILES='./splitit.sh ./poly/getpoly.sh ./7w/pack.sh ./7w/store.lst ./osm/get.sh ./poly/get.sh 27w.sh config.cfg osm2navikey.sh';
 
 for file in $FILES; do \
   if (test -s $file); then \
     sed -e s/setupcountry/$CNTR/g ./$file >> ./$file.1
     mv -f ./$file.1 ./$file
   fi
-chmod 777 ./7w/pack.sh ./osm/get.bat ./poly/get.sh 27w.sh osm2navikey.bat ./splitit.sh ./poly/getpoly.sh
+chmod 777 ./7w/pack.sh ./osm/get.sh ./poly/get.sh 27w.sh osm2navikey.sh ./splitit.sh ./poly/getpoly.sh
 done
